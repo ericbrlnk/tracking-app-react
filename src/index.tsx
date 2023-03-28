eb377447393cc25213e7a3cdd04ff9b1f65b4aa6
lukasz.molinek <lukasz.molinek@ics-ag.de>
2023-03-28 14:46:14 +0200

    deleted javascript test-scripts

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom"
-// Test scripts
-import test from './Test';
-import addWithUnion from './TestUnionType';
-import stringLiterals from './TestStringLiterals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,8 +12,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-test();
-let result = addWithUnion("4", "1");
-console.log(result);
-stringLiterals();
